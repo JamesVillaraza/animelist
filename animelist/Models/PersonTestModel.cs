4cf8c32c20f63de7686cf2d8f1c4e105cee14303
danhrubec <60075606+danhrubec@users.noreply.github.com>
2022-10-29 11:28:52 -0500

    test model

---
+++ animelist/Models/PersonTestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace animelist.Models
+{
+    public class PersonTestModel
+    {
+        public string name {get;set;}
+        public int age {get;set;}
+        public string lastname { get; set;}
+        public string firstname { get; set; }
+
+    }
+}
